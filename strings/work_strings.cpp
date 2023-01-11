74d2e114f2981f25b5e14c720e38efdb16528b21
JhonRmzG <jhonramirezgamboa@gmail.com>
2023-01-10 22:56:20 -0600

    Statements and first simple functions

---
+++ strings/work_strings.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Jonathan Ramirez on 09/01/23.
+//
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string phrase = "This is the best phrase ever.";
+    cout << "Hello World Once Again!" <<endl;
+    cout << phrase.length() << endl;
+    cout << phrase[0] << endl;
+    phrase[0] = 'B';
+    cout << phrase.find("best") << endl;
+    cout << phrase.substr(12, 2);
+    return 0;
+}
