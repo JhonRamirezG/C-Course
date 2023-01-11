66624c3b7874c284df2b2620ea24351909599b20
JhonRmzG <jhonramirezgamboa@gmail.com>
2023-01-10 23:26:24 -0600

    basic calculator

---
+++ calculator/main.cpp
@@ -0,0 +1,20 @@
+//
+// Created by Jonathan Ramirez on 10/01/23.
+//
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    double num1, num2;
+
+    cout << "Enter the first number: ";
+    cin >> num1;
+
+    cout << "Enter the second number: ";
+    cin >> num2;
+
+    cout << num1 + num2;
+
+    return 0;
+}
